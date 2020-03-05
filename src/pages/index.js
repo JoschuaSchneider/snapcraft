c77f6bf9985de60723f2f450e6e02999b36f3f42
JoschuaSchneider <joschua-schneider@web.de>
2020-03-05 10:28:01 +0100

    reordered classnames, fixed mobile layout, added scroll when field overflows

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,59 +52,59 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Snapcraft" />
-      <header className="py-4 px-5 flex flex-col">
-        <a href="/" className="text-3xl text-gray-700 font-semibold">
+      <header className="flex flex-col px-5 py-6">
+        <a href="/" className="text-3xl font-semibold text-gray-700">
           <span className="text-green-500">Snap</span>craft
         </a>
-        <h4 className="text-gray-700 font-semibold mt-1">
+        <h4 className="mt-1 font-semibold text-gray-700">
           Create beautiful inventory screenshots.
         </h4>
       </header>
-      <div className="flex justify-center py-12 px-5 rounded-tl mt-10 rounded-tr">
-        <div className="shadow-2xl rounded overflow-hidden">
+      <div className="flex justify-center px-5 py-12 mt-10 rounded-tl rounded-tr">
+        <div className="overflow-auto scrolling-touch rounded shadow-2xl">
           <CraftingField
             ref={fieldRef}
             onItemClick={(x, y, item) => setItemAt(x, y, currentItem)}
-            width={width}
-            height={height}
+            width={width > 0 ? width : 1}
+            height={height > 0 ? height : 1}
             items={items}
           />
         </div>
       </div>
       {resultImage && <img src={resultImage} />}
-      <div className="flex flex-col rounded mb-2 py-6 px-6">
-        <span className="text-gray-700 font-semibold text-md mb-2">
+      <div className="flex flex-col px-6 py-6 mb-2 rounded">
+        <span className="mb-2 font-semibold text-gray-700 text-md">
           Current Brush
         </span>
         <div className="flex items-center mt-2">
           <div style={{ width: "50px", height: "50px" }}>
             <MinecraftItem
               style={{ width: "50px", height: "50px" }}
-              className="rounded overflow-hidden"
+              className="overflow-hidden rounded"
               name={currentItem}
             />
           </div>
-          <span className="text-gray-700 font-semibold text-md ml-4">
-            <span className="text-green-600">{currentItem || "Clear"}</span>
+          <span className="ml-4 font-semibold text-gray-700 text-md">
+            <span className="text-gray-700">{currentItem || "Clear"}</span>
           </span>
         </div>
       </div>
-      <div className="flex flex-col shadow-xl rounded bg-white py-8 px-8 border-b-4 border-green-600">
-        <div className="flex">
+      <div className="flex flex-col px-8 py-8 bg-white border-b-4 border-green-600 rounded shadow-xl">
+        <div className="flex flex-col -mx-3 md:flex-row">
           <button
-            className="bg-gray-100 text-gray-700 hover:bg-gray-200 py-2 px-4 font-semibold rounded"
+            className="flex-grow px-4 py-2 mx-3 mb-3 font-semibold text-gray-700 bg-gray-100 rounded hover:bg-gray-200"
             onClick={() => setCurrentItem(null)}
           >
             Clear Brush
           </button>
           <button
-            className="bg-red-100 text-red-700 hover:bg-red-200 py-2 px-4 font-semibold rounded ml-3"
+            className="flex-grow px-4 py-2 mx-3 mb-3 font-semibold text-red-700 bg-red-100 rounded hover:bg-red-200"
             onClick={() => setItems({})}
           >
             Clear All
           </button>
           <button
-            className="bg-green-100 text-green-700 hover:bg-green-200 py-2 px-4 font-semibold rounded ml-auto"
+            className="flex-grow px-4 py-2 mx-3 mb-3 font-semibold text-green-700 bg-green-100 rounded hover:bg-green-200"
             onClick={async () => {
               const img = await domtoimage.toPng(fieldRef.current)
               download(img, "snapcraft-snap.png")
@@ -113,8 +113,8 @@ const IndexPage = () => {
             Download Image
           </button>
         </div>
-        <div className="flex -mx-3 mt-4">
-          <label htmlFor="item-search" className="block mx-3 flex-grow">
+        <div className="flex mt-4 -mx-3">
+          <label htmlFor="item-search" className="flex-grow block mx-3">
             <span className="text-gray-700">Item Search</span>
             <ItemSelect
               id="item-search"
@@ -123,64 +123,64 @@ const IndexPage = () => {
             />
           </label>
         </div>
-        <h3 className="text-gray-700 mt-3">Item history</h3>
+        <h3 className="mt-3 text-gray-700">Item history</h3>
         <div className="flex flex-wrap mt-2">
           {recentItems.map(item => (
             <MinecraftItem
               key={item}
               onClick={() => setBrushItem(item)}
               style={{ width: "50px", height: "50px" }}
-              className="rounded cursor-pointer hover:opacity-75 overflow-hidden mr-2 mb-2"
+              className="mb-2 mr-2 overflow-hidden rounded cursor-pointer hover:opacity-75"
               name={item}
             />
           ))}
         </div>
-        <div className="flex -mx-3 mt-3">
-          <label htmlFor="width" className="block mx-3 flex-grow">
+        <div className="flex mt-3 -mx-3">
+          <label htmlFor="width" className="flex-grow block mx-3">
             <span className="text-gray-700">Width</span>
             <input
               min={1}
               type="number"
               id="width"
-              className="form-input block w-full mt-1"
+              className="block w-full mt-1 form-input"
               value={width}
               onChange={e => setWidth(e.target.valueAsNumber)}
             />
           </label>
-          <label htmlFor="width" className="block mx-3 flex-grow">
+          <label htmlFor="width" className="flex-grow block mx-3">
             <span className="text-gray-700">Height</span>
             <input
               min={1}
               type="number"
               id="height"
-              className="form-input block w-full mt-1"
+              className="block w-full mt-1 form-input"
               value={height}
               onChange={e => setHeight(e.target.valueAsNumber)}
             />
           </label>
         </div>
-        <h3 className="text-gray-700 mt-3">Inventory Presets</h3>
-        <div className="flex flex-wrap mt-2 -mb-3">
+        <h3 className="mt-3 text-gray-700">Inventory Presets</h3>
+        <div className="flex flex-col flex-wrap mt-2 -mb-3 md:flex-row">
           <button
-            className="bg-gray-100 text-gray-700 hover:bg-gray-200 py-2 px-4 font-semibold rounded mr-3 mb-3"
+            className="px-4 py-2 mb-3 mr-3 font-semibold text-gray-700 bg-gray-100 rounded hover:bg-gray-200"
             onClick={() => loadPreset(3, 3)}
           >
             Crafting Table (3x3)
           </button>
           <button
-            className="bg-gray-100 text-gray-700 hover:bg-gray-200 py-2 px-4 font-semibold rounded mr-3 mb-3"
+            className="px-4 py-2 mb-3 mr-3 font-semibold text-gray-700 bg-gray-100 rounded hover:bg-gray-200"
             onClick={() => loadPreset(9, 3)}
           >
             Inventory/Chest (9x3)
           </button>
           <button
-            className="bg-gray-100 text-gray-700 hover:bg-gray-200 py-2 px-4 font-semibold rounded mr-3 mb-3"
+            className="px-4 py-2 mb-3 mr-3 font-semibold text-gray-700 bg-gray-100 rounded hover:bg-gray-200"
             onClick={() => loadPreset(9, 6)}
           >
             Double Chest (9x6)
           </button>
           <button
-            className="bg-gray-100 text-gray-700 hover:bg-gray-200 py-2 px-4 font-semibold rounded mr-3 mb-3"
+            className="px-4 py-2 mb-3 mr-3 font-semibold text-gray-700 bg-gray-100 rounded hover:bg-gray-200"
             onClick={() => loadPreset(9, 1)}
           >
             Hotbar (9x1)
@@ -188,7 +188,7 @@ const IndexPage = () => {
         </div>
       </div>
       <div className="flex flex-col items-center mt-16">
-        <span className="text-red-500 mb-3 max-w-2xl text-center">
+        <span className="max-w-2xl mb-3 text-center text-red-500">
           There are some missing items that don't have Images, they will be
           added in the future!
         </span>
